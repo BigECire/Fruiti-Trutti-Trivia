ecd611eb4afe0af62de82403866489fe82f4a24d
BigECire <ericsanders5800@gmail.com>
2019-05-25 22:49:04 -0500

    done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,21 +7,31 @@ $( document ).ready(function() {
         wrong: 0,
         answered: 0,
         theCorrectAnswer: "",
-        questions: ["question1", "question2", "question3"],
+        questions: ["question1", "question2", "question3", "question4", "question5"],
         question1: {
-            question: "HI?",
-            answers:["1", "2", "3", "4"],
-            correctAnswer: "3"
+            question: "Which one of these is not a berry?",
+            answers:["Avocados", "Bananas", "Strawberries", "Watermelon"],
+            correctAnswer: "Strawberries"
         },
         question2: {
-            question: "H?",
-            answers:["11", "22", "33", "44"],
-            correctAnswer: "22"
+            question: "Which one of these are not a member of the rose family?",
+            answers:["Apples", "Peaches", "Raspberries", "Lemon"],
+            correctAnswer: "Lemon"
         },
         question3: {
-            question: "H3?",
-            answers:["111", "222", "333", "444"],
-            correctAnswer: "111"
+            question: "What is the world's most popular fruit?",
+            answers:["Tomatos", "Apples", "Bananas", "Grapes"],
+            correctAnswer: "Tomatos"
+        },
+        question4: {
+            question: "__________ is the study of fruits.",
+            answers:["Olericulture", "Pomology", "Oenology", "Floriculture"],
+            correctAnswer: "Pomology"
+        },
+        question5: {
+            question: "There is a tree called ____________ that sprouts 3 to 7 different fruits in the same tree.",
+            answers:["Wonderland Tree", "Chimera tree", "Fruit Salad Tree", "Cornucopia Tree"],
+            correctAnswer: "Fruit Salad Tree"
         },
         count: function() {
 
@@ -40,12 +50,13 @@ $( document ).ready(function() {
                     console.log("hi")
                     clearInterval(intervalId);
                     trivia.qPhase = true
+                    
                     if(trivia.answered !== trivia.questions.length){
-                    trivia.questionPhase()
-                }
-                else{
-                    trivia.resultsPhase()
-                }
+                        trivia.questionPhase()
+                    }
+                    else{
+                        trivia.resultsPhase()
+                    }
                 }
 
 
@@ -56,15 +67,11 @@ $( document ).ready(function() {
             $("#game").empty();
 
             var div = $("<div>");
-            div.addClass("card w-100");
+            div.addClass("card card-change w-100");
 
-            var Q = $("<h6>")
+            var Q = $("<h4>")
             Q.text(trivia[trivia.questions[trivia.answered]]["question"])
             div.append(Q)
-
-            var br = $("<div> <br> </div>")
-            div.append(br)
-            div.append(br)
 
             for (var i = 0; i < 4; i++){
                 var A = $("<h5>")
@@ -72,7 +79,6 @@ $( document ).ready(function() {
                 A.addClass("Answer A" + (i + 1))
                 A.attr("value", trivia[trivia.questions[trivia.answered]]["answers"][i])
                 div.append(A)
-                div.append(br)
             }
             $("#game").append(div)
 
@@ -95,7 +101,7 @@ $( document ).ready(function() {
             if(answer === trivia[trivia.questions[trivia.answered]]["correctAnswer"]){
                 $("#game").empty();
                 var div = $("<div>");
-                div.addClass("card w-100");
+                div.addClass("card card-change w-100");
 
                 var C = $("<h3>")
                 C.text("Congratulations!")
@@ -106,9 +112,11 @@ $( document ).ready(function() {
                 div.append(Y)
 
                 $("#game").append(div)
+                trivia.correct++
             }
             else{
                $(".A" + (1 + trivia[trivia.questions[trivia.answered]]["answers"].indexOf(trivia[trivia.questions[trivia.answered]]["correctAnswer"]))).attr("style", "background-color: green;")
+               trivia.wrong++
             }
             trivia.time = 3
             $(".clock").text("Time Remaining: " + trivia.time + " Seconds");
@@ -119,18 +127,18 @@ $( document ).ready(function() {
             $("#game").empty();
 
             var div = $("<div>");
-            div.addClass("card w-100");
+            div.addClass("card card-change w-100");
 
             var C = $("<h3>")
             C.text("Your Score")
             div.append(C)
 
             var Y = $("<h5>")
-            Y.text("Correct: ")
+            Y.text("Correct: " + trivia.correct)
             div.append(Y)
 
             var Y = $("<h5>")
-            Y.text("Wrong: ")
+            Y.text("Wrong: " + trivia.wrong)
             div.append(Y)
 
             var C = $("<h4>")
@@ -138,17 +146,24 @@ $( document ).ready(function() {
             div.append(C)
 
             var btn = $("<button>")
+            btn.addClass("btn btn-info")
             btn.text("Replay")
             div.append(btn)
 
             $("#game").append(div)
+            $(".clock").text("Time Remaining: ∞");
 
             $('.container').on("click","button", function(){
                 trivia.reset()
             })
         },
         reset: function() {
-
+            trivia.qPhase = true
+            trivia.correct = 0
+            trivia.wrong = 0
+            trivia.answered = 0
+            clearInterval(intervalId);
+            trivia.questionPhase()
         }
     }
     trivia.questionPhase()
